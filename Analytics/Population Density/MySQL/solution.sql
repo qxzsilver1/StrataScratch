bffeee91422c7ba8995d9ae9c0cdb3df52bf6946
qxzsilver1 <hk442@cornell.edu>
2024-06-17 11:10:01 -0400

    Update solution.sql
    Correct solution to include the country as well.

--- Analytics/Population Density/MySQL/solution.sql
+++ Analytics/Population Density/MySQL/solution.sql
@@ -1,5 +1,5 @@
-select t.city, t.density
-from (select city, population / area as density,
+select t.city, t.country, t.density
+from (select city, country, population / area as density,
 RANK() OVER (order by population / area desc) rank_desc, RANK() OVER (order by population / area ASC) rank_asc
 from cities_population
 where area <> 0) t
